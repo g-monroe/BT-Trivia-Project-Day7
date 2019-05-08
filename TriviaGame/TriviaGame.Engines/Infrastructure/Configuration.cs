08fa30c9d7be7b911acc742a2a03fc666a86a4ce
Daric Teske <daric.teske@buildertrend.com>
2019-05-08 17:21:54 -0500

    templating out the backend

---
+++ TriviaGame/TriviaGame.Engines/Infrastructure/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TriviaGame.Core.Interfaces.Engines;
+using TriviaGame.Engines;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddEngines(this IServiceCollection services)
+        {
+            services.AddTransient<IToDoEngine, ToDoEngine>();
+
+            return services;
+        }
+    }
+}
